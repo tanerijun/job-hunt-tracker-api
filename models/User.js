f466071a07f71825af0e607b2f69d1df0d93433d
TaneriX07 <tanerivince@gmail.com>
2022-05-02 19:39:51 +0800

    Refactor password hash into it's own middleware

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -23,4 +24,12 @@ const UserSchema = new mongoose.Schema({
   },
 })
 
+// Hash password
+// Use old function syntax so that it scope the whole document
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+
+  next()
+})
 module.exports = mongoose.model('User', UserSchema)
