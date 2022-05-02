b5d08caeb287c6b9cfe10fb3755efdd2d7c05447
TaneriX07 <tanerivince@gmail.com>
2022-05-02 19:55:30 +0800

    Integrate JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,17 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   // create user
   const user = await User.create({ ...req.body })
 
-  res.status(StatusCodes.CREATED).json({ user })
+  // create token
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', {
+    expiresIn: '30d',
+  })
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = (req, res) => {
